6a3fd7788b40e6c786039ad3fc7984adf62147f2
POLA <baltazarpilling10@gmail.com>
2024-02-27 14:51:47 -0300

    Work in progress zirpa.cpp

---
+++ src/zirpa.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+string zirpify(string text){
+    
+}
+
+int main(){
+    
+    fstream file;
+    file.open("input.txt", ios::in);
+    if(!file.is_open()){
+        cerr << "Can't open the file \"input.txt\"";
+    }
+
+
+    
+    return 0;
+}
